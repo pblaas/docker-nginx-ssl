e83b65e225eb6466afe742db355e73019214d049
root <root@192.168.2.18>
2016-10-04 17:04:46 +0200

    new keypair

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80:80 -p 443:443 -v $PWD:/etc/nginx/external -v $PWD/html:/usr/share/nginx/html marvambass/nginx-ssl-secure
+docker run -d -p 80:80 -p 443:443 -e 'DH_SIZE=512' -v $PWD:/etc/nginx/external -v $PWD/html:/usr/share/nginx/html marvambass/nginx-ssl-secure
